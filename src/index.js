207076fdfa0d9f67a0a9b7820569b29ba9b1c7ec
rstrauss127 <33469244+rstrauss127@users.noreply.github.com>
2020-06-08 15:21:26 -0400

    Updated index.js
    index.js now shows a list of amenities

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import App from "./App";
+
+const amenitiesList = [
+  "Heated Pool",
+  "Full Sized Washer & Dryer",
+  "Free Wifi",
+  "Cable TV & DVD Player",
+  "Covered & Assigned Parking",
+  "Elevator"
+];
+
+function WelcomeHeader() {
+  return (
+    <div>
+      <h1>Visit Madeira Beach!</h1>
+      <p>
+        Welcome! You can't get any closer to the beach than this! Our three
+        renovated condos have been designed with your comfort in mind. We are
+        located in Villa Madeira, across the street from the Candy Kitchen, and
+        just a short walk to world famous Johns Pass. Our units are all directly
+        on the Gulf with outstanding views of the beach. We provide many
+        amenities that other condo rentals do not, such as top quality linens,
+        including plush bath towels, large beach towels, toys for your children,
+        fully equipped kitchens, and lounges and chairs for the beach.
+      </p>
+    </div>
+  );
+}
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <WelcomeHeader />
+    <App amenities={amenitiesList} />
   </React.StrictMode>,
   rootElement
 );
